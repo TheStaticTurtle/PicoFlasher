3539d6d48a075cb09a6d94a5fc878177fadd1d6b
Samuel <tuglersamuel@gmail.com>
2021-05-10 16:22:41 +0200

    "Stable" test

--- PicoFlasher/Form1.Designer.cs
+++ PicoFlasher/Form1.Designer.cs
@@ -25,12 +25,70 @@ namespace PicoFlasher {
 		/// </summary>
 		private void InitializeComponent() {
 			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Candara Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 6);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(320, 29);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Waiting for a pico in U2F mode";
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 150;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::PicoFlasher.Properties.Resources.pico1;
+			this.pictureBox1.Location = new System.Drawing.Point(13, 59);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(317, 122);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(153, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(28, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = ".......";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(345, 192);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "PicoFlasher";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
--- PicoFlasher/Form1.cs
+++ PicoFlasher/Form1.cs
@@ -1,17 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PicoFlasher {
 	public partial class Form1 : Form {
+
+        String flash_file = null;
+        String uf2_file_path = null;
+
 		public Form1() {
-			InitializeComponent();
+            string[] arguments = Environment.GetCommandLineArgs();
+            if (arguments.Length < 2) {
+                MessageBox.Show("Missing file argument, you need to supply the ELF file as an argument");
+                System.Environment.Exit(1);
+            }
+            flash_file = arguments[1];
+            if (!File.Exists(flash_file)) {
+                MessageBox.Show("The supplied file does not exist");
+                System.Environment.Exit(1);
+            }
+
+            uf2_file_path = flash_file.Substring(0, flash_file.Length - 3)+"uf2";
+            if (!File.Exists(uf2_file_path)) {
+                MessageBox.Show(uf2_file_path + " The supplied file does not exist");
+                System.Environment.Exit(1);
+            }
+
+            InitializeComponent();
 		}
-	}
+
+		private void Form1_Load(object sender, EventArgs e) {
+            timer1.Enabled = true;
+            waitAndFlash();
+        }
+
+		private void timer1_Tick(object sender, EventArgs e) {
+			waitAndFlash();
+		}
+
+		private void waitAndFlash() {
+            String drive_letter = null;
+
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * FROM Win32_DiskDrive")) {
+                collection = searcher.Get();
+                foreach (var device in collection) {
+                    if(device.GetPropertyValue("Model").ToString().Contains("RPI RP2")) {
+                        foreach (ManagementObject partition in new ManagementObjectSearcher("ASSOCIATORS OF {Win32_DiskDrive.DeviceID='" + device.GetPropertyValue("DeviceID").ToString() + "'} WHERE AssocClass = Win32_DiskDriveToDiskPartition").Get()) {
+                            foreach (ManagementObject disk in new ManagementObjectSearcher("ASSOCIATORS OF {Win32_DiskPartition.DeviceID='" + partition["DeviceID"] + "'} WHERE AssocClass = Win32_LogicalDiskToPartition").Get()) {
+                                drive_letter = disk.GetPropertyValue("Name").ToString();
+                            }
+                        }
+                    }
+                }
+            }
+
+            if(drive_letter != null) {
+                label1.Text = "Flashing";
+                label2.Text = "Copy U2F file to " + drive_letter;
+                label1.Location = new Point(this.Width / 2 - label1.Width / 2, label1.Location.Y);
+                label2.Location = new Point(this.Width / 2 - label2.Width / 2, label2.Location.Y);
+
+                File.Copy(uf2_file_path, drive_letter+"\\"+Path.GetFileName(uf2_file_path));
+                System.Environment.Exit(1);
+            }
+        }
+
+        static List<USBDeviceInfo> GetUSBDevices() {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_PnPEntity"))
+                collection = searcher.Get();
+
+            foreach (var device in collection) {
+                devices.Add(new USBDeviceInfo(
+                    (string)device.GetPropertyValue("DeviceID"),
+                    (string)device.GetPropertyValue("PNPDeviceID"),
+                    (string)device.GetPropertyValue("Manufacturer"),
+                    (string)device.GetPropertyValue("Description"),
+                    device
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+    }
+
+    class USBDeviceInfo {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string manufacturer, string description, ManagementBaseObject device) {
+            this.DeviceID = deviceID != null ? deviceID : "";
+            this.PnpDeviceID = pnpDeviceID != null ? deviceID : "";
+            this.Manufacturer = manufacturer != null ? manufacturer : "";
+            this.Description = description != null ? description : "";
+            this.Device = device;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+        public string Manufacturer { get; private set; }
+        public ManagementBaseObject Device { get; private set; }
+    }
 }
